1c5d72dc900d16aa869d8d40629a7077c619024d
NadeeraPereraD <98936438+NadeeraPereraD@users.noreply.github.com>
2025-11-17 14:58:28 +0530

    Created IUserRolesService

--- SoftChoiceApp.API/Interfaces/IUserRolesService.cs
+++ SoftChoiceApp.API/Interfaces/IUserRolesService.cs
@@ -1,6 +1,15 @@
-﻿namespace SoftChoiceApp.API.Interfaces
+﻿using SoftChoiceApp.API.Models.DTOs.UserManagementDTO;
+using SoftChoiceApp.API.Models.Entities;
+
+namespace SoftChoiceApp.API.Interfaces
 {
     public interface IUserRolesService
     {
+        Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> CreateAsync(UserRolesCreateDto dto);
+        Task<(IEnumerable<UserRole> userRoles, string? ErrorMessage, string? SuccessMessage)> GetAllUserRolesAsync();
+        Task<UserRolesResponseDto> GetUserRolesAsync(UserRolesRequestDto request);
+        Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> UpdateAsyncByID(UserRolesUpdateDto dto);
+        Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> SoftDeleteAsyncByID(UserRolesSoftDeleteDto dto);
+        Task<(IEnumerable<UserRole> userRoles, string? ErrorMessage, string? SuccessMessage)> GetAllInactiveUserRolesAsync();
     }
 }
