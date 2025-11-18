3d75d4bc2e0466aacd14b02ce6ba11233e952807
NadeeraPereraD <98936438+NadeeraPereraD@users.noreply.github.com>
2025-11-18 13:27:20 +0530

    Added IUsersRepository

--- SoftChoiceApp.API/Interfaces/IUsersRepository.cs
+++ SoftChoiceApp.API/Interfaces/IUsersRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace SoftChoiceApp.API.Interfaces
+﻿using SoftChoiceApp.API.Models.DTOs.UserManagementDTO;
+using SoftChoiceApp.API.Models.Entities;
+
+namespace SoftChoiceApp.API.Interfaces
 {
     public interface IUsersRepository
     {
-
+        Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> CreateAsync(UsersCreateDto dto);
+        Task<(IEnumerable<User> users, string? ErrorMessage, string? SuccessMessage)> GetAllAsync();
+        Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> UpdateByKeyAsync(UsersUpdateDto dto);
+        Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> SoftDeleteByKeyAsync(UsersSoftDeleteDto dto);
+        Task<(IEnumerable<User> users, string? ErrorMessage, string? SuccessMessage)> GetAllInactiveAsync();
     }
 }
