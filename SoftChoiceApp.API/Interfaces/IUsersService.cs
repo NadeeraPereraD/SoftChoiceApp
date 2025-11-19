9fcbb39f26aa2042afb4a8080099037e11a887b6
NadeeraPereraD <98936438+NadeeraPereraD@users.noreply.github.com>
2025-11-19 13:23:24 +0530

    Added LoginAsync to IUsersService

--- SoftChoiceApp.API/Interfaces/IUsersService.cs
+++ SoftChoiceApp.API/Interfaces/IUsersService.cs
@@ -10,5 +10,6 @@ namespace SoftChoiceApp.API.Interfaces
         Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> UpdateAsyncByID(UsersUpdateDto dto);
         Task<(bool IsSuccess, string? ErrorMessage, string? SuccessMessage)> SoftDeleteAsyncByID(UsersSoftDeleteDto dto);
         Task<(IEnumerable<UserWithRolesDto> users, string? ErrorMessage, string? SuccessMessage)> GetAllInactiveUserRolesAsync();
+        Task<(bool IsSuccess, string? ErrorMessage, string? Token)> LoginAsync(LoginRequestDto dto);
     }
 }
